84d1fcf1e59571f81364cede1db5c792b3519512
RadchenkoArtem2023 <143721793+RadchenkoArtem2023@users.noreply.github.com>
2024-09-12 15:42:31 +0300

    add_task

--- src/db/models/water.js
+++ src/db/models/water.js
@@ -1,17 +1,45 @@
-import { Schema, model } from 'mongoose';
+//import { Schema, model } from 'mongoose';
 
-const aquaSchema = new Schema(
+//const aquaSchema = new Schema(
+//    {
+//        date: { type: Date, required: true },
+//        amount: { type: Number, required: true },
+//        userId: { type: Schema.Types.ObjectId, ref: 'users', required: true },
+//    },
+//    {
+//        timestamps: false,
+//        versionKey: false,
+//    },
+//);
+
+//const Water = model('water', aquaSchema);
+
+//export default Water;
+
+import mongoose from 'mongoose';
+
+const getCurrentDate = () => {
+    const now = new Date();
+    return new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate(),
+        now.getHours(),
+        now.getMinutes(),
+    );
+};
+
+const waterRecordSchema = new mongoose.Schema(
     {
-        date: { type: Date, required: true },
         amount: { type: Number, required: true },
-        userId: { type: Schema.Types.ObjectId, ref: 'users', required: true },
+        date: { type: Date, required: true, default: getCurrentDate },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
     },
-    {
-        timestamps: false,
-        versionKey: false,
-    },
+    { versionKey: false, timestamps: true },
 );
 
-const Water = model('water', aquaSchema);
-
-export default Water;
+export default mongoose.model('WaterRecord', waterRecordSchema);
