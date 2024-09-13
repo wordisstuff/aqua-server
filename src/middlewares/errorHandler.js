df3a2250e8ce650bdd62d37b31a34979b23a9d98
RadchenkoArtem2023 <143721793+RadchenkoArtem2023@users.noreply.github.com>
2024-09-13 10:10:03 +0300

    validation_fixed

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -11,7 +11,7 @@ export const errorHandler = (error, req, res, next) => {
     }
     res.status(500).json({
         status: 500,
-        message: 'Somthing went wrong!!!',
+        message: 'Something went wrong!!!',
         data: { message: error.message },
     });
 };
