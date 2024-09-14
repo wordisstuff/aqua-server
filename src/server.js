0d687da3e2753ed414f0d125497ac45c2b6d28a2
Volodymyr180688 <shurik180688@gmail.com>
2024-09-14 18:39:55 +0300

    documentation for auth routers

--- src/server.js
+++ src/server.js
@@ -4,12 +4,13 @@ import pino from 'pino-http';
 import { pinoSettings } from './constants/constants.js';
 import { authDb } from './constants/index.js';
 import { notFindMiddleware } from './middlewares/notFindMiddleware.js';
+import path from 'node:path';
 
 import Router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
 import helloMark from './utils/helloMark.js';
-// import swaggerDocs from './middlewares/swaggerDocs.js';
+import swaggerDocs from './middlewares/swaggerDocs.js';
 
 export const setupServer = () => {
     const app = express();
@@ -18,8 +19,8 @@ export const setupServer = () => {
     app.use(cors());
     app.use(cookieParser());
 
-    //   app.use('/api-docs', swaggerDocs());
-    //   app.use('/uploads', express.static(UPLOAD_DIR));
+    app.use('/api-docs', swaggerDocs());
+    app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
     app.get('/', (req, res) => res.send(helloMark()));
     app.use(Router);
