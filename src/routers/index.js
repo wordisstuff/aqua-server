db5718619092e8fcb3e24bed33b0dfa859892ba1
wordisstuff <wordisstuff@gmail.com>
2024-11-27 06:43:20 +0000

    get users

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import waterRouter from './water.js';
 import authRouter from './auth.js';
 import userRouter from './user.js';
+import usersRouter from './users.js';
 import authenticate from '../middlewares/authenticate.js';
 import { jsonParser } from '../constants/constants.js';
 
@@ -10,6 +11,6 @@ const router = Router();
 router.use('/auth', authRouter);
 router.use('/user', authenticate, userRouter);
 router.use('/water', authenticate, jsonParser, waterRouter);
-router.use('/users',jsonParser,)
+router.use('/users',jsonParser,usersRouter)
 
 export default router;
